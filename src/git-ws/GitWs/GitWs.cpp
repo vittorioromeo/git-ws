6ae92cd85953ee7fb704eb32653cbb8b8460c5bb
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-07-22 23:03:58 +0200

    automated submodule update

--- src/git-ws/GitWs/GitWs.cpp
+++ src/git-ws/GitWs/GitWs.cpp
@@ -183,7 +183,6 @@ namespace gitws
 			{
 				runShInRepos(currentRepoPaths, "git submodule update --recursive");
 				runShInRepos(currentRepoPaths, "git submodule foreach git pull origin master --recurse-submodules");
-				runShInRepos(currentRepoPaths, "git submodule update --recursive");
 			}
 			if(arg.get() == "au") runShInRepos(currentRepoPaths, "git commit -am 'automated submodule update'; git push");
 		};

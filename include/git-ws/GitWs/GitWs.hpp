7287d44643485ee66861d29334bf31e84b808a4b
Vittorio Romeo <vittorio.romeo@outlook.com>
2015-10-06 12:35:08 +0200

    automatic clang-format run

--- include/git-ws/GitWs/GitWs.hpp
+++ include/git-ws/GitWs/GitWs.hpp
@@ -10,171 +10,183 @@
 
 namespace gitws
 {
-class Repo
-{
-public:
-    enum class Status
+    class Repo
     {
-        None,
-        CanCommit,
-        DirtySM
-    };
+    public:
+        enum class Status
+        {
+            None,
+            CanCommit,
+            DirtySM
+        };
 
-public:
-    inline auto run(const std::string& mStr) const
-    {
-        std::vector<std::string> result;
-        std::string toRun{"(cd " + path.getStr() + " && " + mStr + ")"}, file;
-        FILE* pipe{popen(toRun.c_str(), "r")};
-        char buffer[1000];
-        while(fgets(buffer, sizeof(buffer), pipe) != NULL) {
-            file = buffer;
-            result.emplace_back(file.substr(0, file.size() - 1));
+    public:
+        inline auto run(const std::string& mStr) const
+        {
+            std::vector<std::string> result;
+            std::string toRun{"(cd " + path.getStr() + " && " + mStr + ")"},
+                file;
+            FILE* pipe{popen(toRun.c_str(), "r")};
+            char buffer[1000];
+            while(fgets(buffer, sizeof(buffer), pipe) != NULL)
+            {
+                file = buffer;
+                result.emplace_back(file.substr(0, file.size() - 1));
+            }
+            pclose(pipe);
+
+            return result;
         }
-        pclose(pipe);
 
-        return result;
-    }
+    private:
+        ssvufs::Path path;
+        std::string branch;
 
-private:
-    ssvufs::Path path;
-    std::string branch;
+        inline auto runGetBranch() const
+        {
+            return run("git rev-parse --abbrev-ref HEAD")[0];
+        }
+        inline auto runRevList(const std::string& mBranch) const
+        {
+            return run("git rev-list HEAD...origin/" + mBranch +
+                       " --ignore-submodules --count")[0];
+        }
+        inline auto runGetFetch() const
+        {
+            return run("git fetch --dry-run 2>&1");
+        }
+        inline auto runGetFetchSM() const
+        {
+            return run("git submodule foreach git fetch --dry-run 2>&1");
+        }
+        inline bool runHasDiffIndex(bool mIgnoreSubmodules = false) const
+        {
+            return run("git diff-index --name-only"s +
+                       (mIgnoreSubmodules ? " --ignore-submodules" : "") +
+                       " HEAD --")
+                .empty();
+        }
 
-    inline auto runGetBranch() const
-    {
-        return run("git rev-parse --abbrev-ref HEAD")[0];
-    }
-    inline auto runRevList(const std::string& mBranch) const
-    {
-        return run("git rev-list HEAD...origin/" + mBranch +
-                   " --ignore-submodules --count")[0];
-    }
-    inline auto runGetFetch() const { return run("git fetch --dry-run 2>&1"); }
-    inline auto runGetFetchSM() const
-    {
-        return run("git submodule foreach git fetch --dry-run 2>&1");
-    }
-    inline bool runHasDiffIndex(bool mIgnoreSubmodules = false) const
-    {
-        return run("git diff-index --name-only"s +
-                   (mIgnoreSubmodules ? " --ignore-submodules" : "") +
-                   " HEAD --")
-        .empty();
-    }
+    public:
+        inline Repo(const ssvufs::Path& mPath)
+            : path{mPath}, branch{runGetBranch()}
+        {
+        }
 
-public:
-    inline Repo(const ssvufs::Path& mPath) : path{mPath}, branch{runGetBranch()}
-    {
-    }
-
-    inline void runSMPull() const
-    {
-        run(R"(git submodule update --recursive --remote --init &&
+        inline void runSMPull() const
+        {
+            run(R"(git submodule update --recursive --remote --init &&
 					git submodule foreach git reset --hard &&
 					git submodule foreach git checkout origin master &&
 					git submodule foreach git rebase origin master &&
 					git submodule foreach git pull -f origin master --recurse-submodules)");
-    }
-    inline void runSMPush() const
-    {
-        run(R"(git commit -am 'automated submodule update' && git push)");
-    }
-
-    inline const auto& getPath() const { return path; }
-    inline const auto& getBranch() const { return branch; }
-
-    inline bool canPush() const { return ssvu::sToInt(runRevList(branch)) > 0; }
-    inline bool canPull() const { return !runGetFetch().empty(); }
-    inline bool canCommit() const { return !runHasDiffIndex(true); }
-    inline auto getStatus(bool mIgnoreSubmodules) const
-    {
-        if(!runHasDiffIndex(true)) return Status::CanCommit;
-        if(!mIgnoreSubmodules && !runHasDiffIndex(false))
-            return Status::DirtySM;
-        return Status::None;
-    }
-    inline bool getSubmodulesBehind() const
-    {
-        for(const auto& s : runGetFetchSM()) {
-            if(ssvu::beginsWith(s, "Entering")) continue;
-            return true;
         }
-        return false;
-    }
-};
-
-class GitWs
-{
-private:
-    std::vector<Repo> repos;
-    ssvucl::Ctx ctx;
-
-    inline auto getBehindSMRepos() const
-    {
-        std::vector<Repo> result;
-        for(const auto& rd : repos) {
-            const auto& cs(rd.getStatus(false));
-            if((rd.getSubmodulesBehind() || cs == Repo::Status::DirtySM) &&
-               cs != Repo::Status::CanCommit && !rd.canPush())
-                result.emplace_back(rd);
+        inline void runSMPush() const
+        {
+            run(R"(git commit -am 'automated submodule update' && git push)");
         }
-        return result;
-    }
-    inline auto getChangedRepos(bool mIgnoreSubmodules) const
-    {
-        std::vector<Repo> result;
-        for(const auto& rd : repos)
-            if(rd.getStatus(mIgnoreSubmodules) == Repo::Status::CanCommit)
-                result.emplace_back(rd);
-        return result;
-    }
-    inline auto getAheadRepos() const
-    {
-        std::vector<Repo> result;
-        for(const auto& rd : repos)
-            if(rd.canPush()) result.emplace_back(rd);
-        return result;
-    }
 
-    void runInRepos(const std::vector<Repo>& mRepos,
-    const std::string& mCommand, bool mPrintEmpty = false);
+        inline const auto& getPath() const { return path; }
+        inline const auto& getBranch() const { return branch; }
 
-    void initCmdHelp();
-    void initCmdPush();
-    void initCmdPull();
-    void initCmdSubmodule();
-    void initCmdStatus();
-    void initCmdGitg();
-    void initCmdDo();
-    void initCmdQuery();
+        inline bool canPush() const
+        {
+            return ssvu::sToInt(runRevList(branch)) > 0;
+        }
+        inline bool canPull() const { return !runGetFetch().empty(); }
+        inline bool canCommit() const { return !runHasDiffIndex(true); }
+        inline auto getStatus(bool mIgnoreSubmodules) const
+        {
+            if(!runHasDiffIndex(true)) return Status::CanCommit;
+            if(!mIgnoreSubmodules && !runHasDiffIndex(false))
+                return Status::DirtySM;
+            return Status::None;
+        }
+        inline bool getSubmodulesBehind() const
+        {
+            for(const auto& s : runGetFetchSM())
+            {
+                if(ssvu::beginsWith(s, "Entering")) continue;
+                return true;
+            }
+            return false;
+        }
+    };
 
-    inline void initRepoDatas()
+    class GitWs
     {
-        for(auto& p :
-        ssvufs::getScan<ssvufs::Mode::Single, ssvufs::Type::Folder>("./"))
-            if(ssvufs::Path{p + "/.git/"}.exists<ssvufs::Type::Folder>())
-                repos.emplace_back(p);
-    }
-    inline void initCmds()
-    {
-        initCmdHelp();
-        initCmdPush();
-        initCmdPull();
-        initCmdSubmodule();
-        initCmdStatus();
-        initCmdGitg();
-        initCmdDo();
-        initCmdQuery();
-    }
+    private:
+        std::vector<Repo> repos;
+        ssvucl::Ctx ctx;
 
-public:
-    inline GitWs(int mArgCount, char* mArgValues[])
-    {
-        initRepoDatas();
-        initCmds();
-        ctx.process(mArgCount, mArgValues);
-    }
-};
+        inline auto getBehindSMRepos() const
+        {
+            std::vector<Repo> result;
+            for(const auto& rd : repos)
+            {
+                const auto& cs(rd.getStatus(false));
+                if((rd.getSubmodulesBehind() || cs == Repo::Status::DirtySM) &&
+                    cs != Repo::Status::CanCommit && !rd.canPush())
+                    result.emplace_back(rd);
+            }
+            return result;
+        }
+        inline auto getChangedRepos(bool mIgnoreSubmodules) const
+        {
+            std::vector<Repo> result;
+            for(const auto& rd : repos)
+                if(rd.getStatus(mIgnoreSubmodules) == Repo::Status::CanCommit)
+                    result.emplace_back(rd);
+            return result;
+        }
+        inline auto getAheadRepos() const
+        {
+            std::vector<Repo> result;
+            for(const auto& rd : repos)
+                if(rd.canPush()) result.emplace_back(rd);
+            return result;
+        }
+
+        void runInRepos(const std::vector<Repo>& mRepos,
+            const std::string& mCommand, bool mPrintEmpty = false);
+
+        void initCmdHelp();
+        void initCmdPush();
+        void initCmdPull();
+        void initCmdSubmodule();
+        void initCmdStatus();
+        void initCmdGitg();
+        void initCmdDo();
+        void initCmdQuery();
+
+        inline void initRepoDatas()
+        {
+            for(auto& p :
+                ssvufs::getScan<ssvufs::Mode::Single, ssvufs::Type::Folder>(
+                    "./"))
+                if(ssvufs::Path{p + "/.git/"}.exists<ssvufs::Type::Folder>())
+                    repos.emplace_back(p);
+        }
+        inline void initCmds()
+        {
+            initCmdHelp();
+            initCmdPush();
+            initCmdPull();
+            initCmdSubmodule();
+            initCmdStatus();
+            initCmdGitg();
+            initCmdDo();
+            initCmdQuery();
+        }
+
+    public:
+        inline GitWs(int mArgCount, char* mArgValues[])
+        {
+            initRepoDatas();
+            initCmds();
+            ctx.process(mArgCount, mArgValues);
+        }
+    };
 }
 
 #endif
--- src/git-ws/GitWs/GitWs.cpp
+++ src/git-ws/GitWs/GitWs.cpp
@@ -12,245 +12,260 @@ using namespace ssvucl;
 
 namespace gitws
 {
-string getBriefHelp(const Cmd& mCmd)
-{
-    return mCmd.getNamesStr() + " " + mCmd.getStr<EType::Arg>() + " " +
-           mCmd.getStr<EType::ArgOpt>() + " " + mCmd.getStr<EType::Flag>() +
-           " " + mCmd.getStr<EType::ArgPack>();
-}
-
-void GitWs::runInRepos(
-const vector<Repo>& mRepos, const string& mCommand, bool mPrintEmpty)
-{
-    for(const auto& r : mRepos) {
-        vector<string> files{r.run(mCommand)};
-        if(files.empty() && !mPrintEmpty) continue;
-
-        lo(r.getPath()) << "\n|\n";
-        for(auto& f : files) lo() << "| -> " << f << "\n";
-        lo() << "|\nL" << hr(-1) << "\n\n";
+    string getBriefHelp(const Cmd& mCmd)
+    {
+        return mCmd.getNamesStr() + " " + mCmd.getStr<EType::Arg>() + " " +
+               mCmd.getStr<EType::ArgOpt>() + " " + mCmd.getStr<EType::Flag>() +
+               " " + mCmd.getStr<EType::ArgPack>();
     }
-}
 
-void GitWs::initCmdHelp()
-{
-    auto& cmd(ctx.create({"?", "help"}));
-    cmd.setDesc("Show help for all commands or a single command.");
-
-    auto& argOpt(cmd.create<ArgOpt<string>>(""));
-    argOpt.setName("Command name");
-    argOpt.setBriefDesc("Name of the command to get help for.");
-    argOpt.setDesc("Leave blank to get general help.");
-
-    auto& flagVerbose(cmd.create<Flag>("v", "verbose"));
-    flagVerbose.setBriefDesc("Verbose general help?");
-
-    cmd += [&]
+    void GitWs::runInRepos(
+        const vector<Repo>& mRepos, const string& mCommand, bool mPrintEmpty)
     {
-        if(!argOpt) {
-            lo("git-ws help") << "\n\n";
-            for(const auto& c : ctx.getCmds())
-                lo() << getBriefHelp(*c) << "\n"
-                     << (flagVerbose ? c->getHelpStr() : "");
-            lo() << "\n";
-        }
-        else
+        for(const auto& r : mRepos)
         {
-            auto& c(ctx.findCmd(argOpt.get()));
-            lo() << "\n" << getBriefHelp(c) << "\n" << c.getHelpStr();
+            vector<string> files{r.run(mCommand)};
+            if(files.empty() && !mPrintEmpty) continue;
+
+            lo(r.getPath()) << "\n|\n";
+            for(auto& f : files) lo() << "| -> " << f << "\n";
+            lo() << "|\nL" << hr(-1) << "\n\n";
         }
+    }
 
-        lo().flush();
-    };
-}
-void GitWs::initCmdPush()
-{
-    auto& cmd(ctx.create({"push"}));
-    cmd.setDesc("Pushes every git repo.");
-
-    auto& flagForce(cmd.create<Flag>("f", "force"));
-    flagForce.setBriefDesc("Forced push?");
-
-    auto& flagAll(cmd.create<Flag>("a", "all"));
-    flagAll.setBriefDesc(
-    "Run the command in all repos (even non-ahead ones), for all branches.");
-
-    cmd += [&]
+    void GitWs::initCmdHelp()
     {
-        for(const auto& rd : repos) {
-            if(!flagAll && !rd.canPush()) continue;
+        auto& cmd(ctx.create({"?", "help"}));
+        cmd.setDesc("Show help for all commands or a single command.");
 
-            string toRun{"git push"};
-            if(flagAll) toRun += " --all";
-            if(flagForce) toRun += " -f";
-            if(!flagAll) toRun += " origin " + rd.getBranch();
-            rd.run(toRun);
-        }
-    };
-}
-void GitWs::initCmdPull()
-{
-    auto& cmd(ctx.create({"pull"}));
-    cmd.setDesc("Pulls every git repo.");
+        auto& argOpt(cmd.create<ArgOpt<string>>(""));
+        argOpt.setName("Command name");
+        argOpt.setBriefDesc("Name of the command to get help for.");
+        argOpt.setDesc("Leave blank to get general help.");
 
-    auto& flagStash(cmd.create<Flag>("s", "stash"));
-    flagStash.setBriefDesc("Stash all changes before pulling?");
+        auto& flagVerbose(cmd.create<Flag>("v", "verbose"));
+        flagVerbose.setBriefDesc("Verbose general help?");
 
-    auto& flagForce(cmd.create<Flag>("f", "force-checkout"));
-    flagForce.setBriefDesc("Run a force checkout before pulling?");
-
-    auto& flagChanged(cmd.create<Flag>("c", "changed-only"));
-    flagChanged.setBriefDesc(
-    "Run the command only in folders where repos have changes?");
-
-    cmd += [&]
-    {
-        auto currentRepos(flagChanged ? getChangedRepos(true) : repos);
-        if(flagStash) runInRepos(currentRepos, "git stash");
-        if(flagForce) runInRepos(currentRepos, "git checkout -f");
-        runInRepos(currentRepos, "git pull");
-    };
-}
-void GitWs::initCmdSubmodule()
-{
-    auto& cmd(ctx.create({"sub", "submodule"}));
-    cmd.setDesc(
-    "Work with git submodules in every repo with dirty submodules.");
-
-    auto& arg(cmd.create<Arg<string>>());
-    arg.setName("Action");
-    arg.setBriefDesc(
-    "Action to run for every submodule. Can be 'pull' or 'au'.");
-    arg.setDesc(
-    "'push' recursively pushes all submodules to the remote\n'pull' "
-    "recursively pulls the latest submodules from the remote, discarding any "
-    "change.\n'au' calls 'pull' then pushes in succession.");
-
-    auto& flagAll(cmd.create<Flag>("a", "all"));
-    flagAll.setBriefDesc("Run the command in all repos?");
-
-    cmd += [&]
-    {
-        auto currentRepos(flagAll ? repos : getBehindSMRepos());
-
-        if(arg.get() == "pull" || arg.get() == "au")
-            for(const auto& r : currentRepos) r.runSMPull();
-        if(arg.get() == "push" || arg.get() == "au")
-            for(const auto& r : currentRepos) r.runSMPush();
-    };
-}
-void GitWs::initCmdStatus()
-{
-    auto& cmd(ctx.create({"st", "status"}));
-    cmd.setDesc("Prints the status of all repos.");
-
-    auto& showAllFlag(cmd.create<Flag>("a", "all"));
-    showAllFlag.setBriefDesc("Print empty messages?");
-
-    cmd += [&]
-    {
-        runInRepos(
-        repos, "git status -s --ignore-submodules=dirty", showAllFlag);
-    };
-}
-void GitWs::initCmdGitg()
-{
-    auto& cmd(ctx.create({"gitg"}));
-    cmd.setDesc("Open the gitg gui application in every repo folder.");
-
-    auto& flagAll(cmd.create<Flag>("a", "all"));
-    flagAll.setBriefDesc("Run the command in all repos?");
-
-    cmd += [&]
-    {
-        auto currentRepos(flagAll ? repos : getChangedRepos(true));
-        runInRepos(currentRepos, "gitg -c&");
-    };
-}
-void GitWs::initCmdDo()
-{
-    auto& cmd(ctx.create({"do"}));
-    cmd.setDesc("Runs a shell command in every repo folder.");
-
-    auto& arg(cmd.create<Arg<string>>());
-    arg.setName("Command to run");
-    arg.setBriefDesc(
-    "This is the command that will be called in every repo folder.");
-    arg.setDesc("Consider wrapping a more complex command with quotes.");
-
-    auto& flagChanged(cmd.create<Flag>("c", "changed-only"));
-    flagChanged.setBriefDesc(
-    "Run the command only in folders where repos have changes?");
-
-    auto& flagAhead(cmd.create<Flag>("a", "ahead-only"));
-    flagAhead.setBriefDesc(
-    "Run the command only in folders where repos are ahead of the remote?");
-
-    cmd += [&]
-    {
-        if(flagChanged && flagAhead) {
-            lo() << "-c and -a are mutually exclusive"
-                 << "\n";
-            return;
-        }
-
-        auto currentRepos(flagChanged ? getChangedRepos(true) : repos);
-        if(flagAhead) currentRepos = getAheadRepos();
-
-        runInRepos(currentRepos, arg.get());
-    };
-}
-void GitWs::initCmdQuery()
-{
-    auto& cmd(ctx.create({"query"}));
-    cmd.setDesc(
-    "Queries the status of all the repos, returning whether they are changed "
-    "or ahead.");
-
-    auto& flagDisplayAll(cmd.create<Flag>("a", "display-all"));
-    flagDisplayAll.setBriefDesc(
-    "Display status of all repositories, even the ones without changes.");
-
-    auto& flagCheckSubmodules(cmd.create<Flag>("s", "check-submodules"));
-    flagCheckSubmodules.setBriefDesc(
-    "Check if submodules are outdated or dirty. (Very time consuming)");
-
-    cmd += [&]
-    {
-        for(const auto& rd : repos) {
-            bool foundAnything{false};
-
-            ostringstream s;
-            s << setw(25) << left << rd.getPath() << setw(15)
-              << " ~ " + rd.getBranch() << flush;
-
+        cmd += [&]
+        {
+            if(!argOpt)
             {
-                if(rd.getStatus(true) == Repo::Status::CanCommit) {
-                    s << setw(15) << left << " (can commit)";
-                    foundAnything = true;
-                }
-                if(rd.canPush()) {
-                    s << setw(15) << left << " (can push)";
-                    foundAnything = true;
-                }
-                if(rd.canPull()) {
-                    s << setw(15) << left << " (can pull)";
-                    foundAnything = true;
-                }
-
-                if(!flagCheckSubmodules) continue;
-                if(rd.getStatus(false) == Repo::Status::DirtySM) {
-                    s << setw(15) << left << " (dirty submodules)";
-                    foundAnything = true;
-                }
-                if(rd.getSubmodulesBehind()) {
-                    s << setw(15) << left << " (outdated submodules)";
-                    foundAnything = true;
-                }
+                lo("git-ws help") << "\n\n";
+                for(const auto& c : ctx.getCmds())
+                    lo() << getBriefHelp(*c) << "\n"
+                         << (flagVerbose ? c->getHelpStr() : "");
+                lo() << "\n";
+            }
+            else
+            {
+                auto& c(ctx.findCmd(argOpt.get()));
+                lo() << "\n" << getBriefHelp(c) << "\n" << c.getHelpStr();
             }
 
-            if(foundAnything || flagDisplayAll) cout << s.str() << "\n";
-        }
-    };
-}
+            lo().flush();
+        };
+    }
+    void GitWs::initCmdPush()
+    {
+        auto& cmd(ctx.create({"push"}));
+        cmd.setDesc("Pushes every git repo.");
+
+        auto& flagForce(cmd.create<Flag>("f", "force"));
+        flagForce.setBriefDesc("Forced push?");
+
+        auto& flagAll(cmd.create<Flag>("a", "all"));
+        flagAll.setBriefDesc(
+            "Run the command in all repos (even non-ahead ones), for all "
+            "branches.");
+
+        cmd += [&]
+        {
+            for(const auto& rd : repos)
+            {
+                if(!flagAll && !rd.canPush()) continue;
+
+                string toRun{"git push"};
+                if(flagAll) toRun += " --all";
+                if(flagForce) toRun += " -f";
+                if(!flagAll) toRun += " origin " + rd.getBranch();
+                rd.run(toRun);
+            }
+        };
+    }
+    void GitWs::initCmdPull()
+    {
+        auto& cmd(ctx.create({"pull"}));
+        cmd.setDesc("Pulls every git repo.");
+
+        auto& flagStash(cmd.create<Flag>("s", "stash"));
+        flagStash.setBriefDesc("Stash all changes before pulling?");
+
+        auto& flagForce(cmd.create<Flag>("f", "force-checkout"));
+        flagForce.setBriefDesc("Run a force checkout before pulling?");
+
+        auto& flagChanged(cmd.create<Flag>("c", "changed-only"));
+        flagChanged.setBriefDesc(
+            "Run the command only in folders where repos have changes?");
+
+        cmd += [&]
+        {
+            auto currentRepos(flagChanged ? getChangedRepos(true) : repos);
+            if(flagStash) runInRepos(currentRepos, "git stash");
+            if(flagForce) runInRepos(currentRepos, "git checkout -f");
+            runInRepos(currentRepos, "git pull");
+        };
+    }
+    void GitWs::initCmdSubmodule()
+    {
+        auto& cmd(ctx.create({"sub", "submodule"}));
+        cmd.setDesc(
+            "Work with git submodules in every repo with dirty submodules.");
+
+        auto& arg(cmd.create<Arg<string>>());
+        arg.setName("Action");
+        arg.setBriefDesc(
+            "Action to run for every submodule. Can be 'pull' or 'au'.");
+        arg.setDesc(
+            "'push' recursively pushes all submodules to the remote\n'pull' "
+            "recursively pulls the latest submodules from the remote, "
+            "discarding any "
+            "change.\n'au' calls 'pull' then pushes in succession.");
+
+        auto& flagAll(cmd.create<Flag>("a", "all"));
+        flagAll.setBriefDesc("Run the command in all repos?");
+
+        cmd += [&]
+        {
+            auto currentRepos(flagAll ? repos : getBehindSMRepos());
+
+            if(arg.get() == "pull" || arg.get() == "au")
+                for(const auto& r : currentRepos) r.runSMPull();
+            if(arg.get() == "push" || arg.get() == "au")
+                for(const auto& r : currentRepos) r.runSMPush();
+        };
+    }
+    void GitWs::initCmdStatus()
+    {
+        auto& cmd(ctx.create({"st", "status"}));
+        cmd.setDesc("Prints the status of all repos.");
+
+        auto& showAllFlag(cmd.create<Flag>("a", "all"));
+        showAllFlag.setBriefDesc("Print empty messages?");
+
+        cmd += [&]
+        {
+            runInRepos(
+                repos, "git status -s --ignore-submodules=dirty", showAllFlag);
+        };
+    }
+    void GitWs::initCmdGitg()
+    {
+        auto& cmd(ctx.create({"gitg"}));
+        cmd.setDesc("Open the gitg gui application in every repo folder.");
+
+        auto& flagAll(cmd.create<Flag>("a", "all"));
+        flagAll.setBriefDesc("Run the command in all repos?");
+
+        cmd += [&]
+        {
+            auto currentRepos(flagAll ? repos : getChangedRepos(true));
+            runInRepos(currentRepos, "gitg -c&");
+        };
+    }
+    void GitWs::initCmdDo()
+    {
+        auto& cmd(ctx.create({"do"}));
+        cmd.setDesc("Runs a shell command in every repo folder.");
+
+        auto& arg(cmd.create<Arg<string>>());
+        arg.setName("Command to run");
+        arg.setBriefDesc(
+            "This is the command that will be called in every repo folder.");
+        arg.setDesc("Consider wrapping a more complex command with quotes.");
+
+        auto& flagChanged(cmd.create<Flag>("c", "changed-only"));
+        flagChanged.setBriefDesc(
+            "Run the command only in folders where repos have changes?");
+
+        auto& flagAhead(cmd.create<Flag>("a", "ahead-only"));
+        flagAhead.setBriefDesc(
+            "Run the command only in folders where repos are ahead of the "
+            "remote?");
+
+        cmd += [&]
+        {
+            if(flagChanged && flagAhead)
+            {
+                lo() << "-c and -a are mutually exclusive"
+                     << "\n";
+                return;
+            }
+
+            auto currentRepos(flagChanged ? getChangedRepos(true) : repos);
+            if(flagAhead) currentRepos = getAheadRepos();
+
+            runInRepos(currentRepos, arg.get());
+        };
+    }
+    void GitWs::initCmdQuery()
+    {
+        auto& cmd(ctx.create({"query"}));
+        cmd.setDesc(
+            "Queries the status of all the repos, returning whether they are "
+            "changed "
+            "or ahead.");
+
+        auto& flagDisplayAll(cmd.create<Flag>("a", "display-all"));
+        flagDisplayAll.setBriefDesc(
+            "Display status of all repositories, even the ones without "
+            "changes.");
+
+        auto& flagCheckSubmodules(cmd.create<Flag>("s", "check-submodules"));
+        flagCheckSubmodules.setBriefDesc(
+            "Check if submodules are outdated or dirty. (Very time consuming)");
+
+        cmd += [&]
+        {
+            for(const auto& rd : repos)
+            {
+                bool foundAnything{false};
+
+                ostringstream s;
+                s << setw(25) << left << rd.getPath() << setw(15)
+                  << " ~ " + rd.getBranch() << flush;
+
+                {
+                    if(rd.getStatus(true) == Repo::Status::CanCommit)
+                    {
+                        s << setw(15) << left << " (can commit)";
+                        foundAnything = true;
+                    }
+                    if(rd.canPush())
+                    {
+                        s << setw(15) << left << " (can push)";
+                        foundAnything = true;
+                    }
+                    if(rd.canPull())
+                    {
+                        s << setw(15) << left << " (can pull)";
+                        foundAnything = true;
+                    }
+
+                    if(!flagCheckSubmodules) continue;
+                    if(rd.getStatus(false) == Repo::Status::DirtySM)
+                    {
+                        s << setw(15) << left << " (dirty submodules)";
+                        foundAnything = true;
+                    }
+                    if(rd.getSubmodulesBehind())
+                    {
+                        s << setw(15) << left << " (outdated submodules)";
+                        foundAnything = true;
+                    }
+                }
+
+                if(foundAnything || flagDisplayAll) cout << s.str() << "\n";
+            }
+        };
+    }
 }
